a835cb4995c745afd533b3a99d28eedd8d9eaefc
Sergey Kudashev <kudashevs@gmail.com>
2025-02-14 00:35:15 +0300

    Add eslint.config.js file

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: ['build/**/*', 'coverage/**/*', 'node_modules/**/*'],
+  },
+  {
+    languageOptions: {
+      globals: {},
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      semi: ['warn', 'always'],
+      'no-useless-escape': 'warn',
+      'no-console': 'error',
+      'prefer-const': 'error',
+    },
+  },
+];
