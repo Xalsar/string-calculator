9a63e0a59d9b484b517286432a1ab7af5747f9cd
Sergey Kudashev <kudashevs@gmail.com>
2025-03-20 19:52:51 +0100

    Update Calculator.ts add multiple delimiters of different length

--- tests/Calculator.test.ts
+++ tests/Calculator.test.ts
@@ -55,4 +55,8 @@ describe("Calculator", () => {
   test("should allow multiple delimiters", () => {
     expect(calculator.add('//[*][%]\n1*2%3')).toBe(6);
   });
+
+  test("should allow multiple delimiters of different length", () => {
+    expect(calculator.add('//[*][%^%][!]\n1%^%2*3!4')).toBe(10);
+  });
 });
